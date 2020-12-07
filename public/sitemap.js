cbf38a7b78f88baf8499706b0cd34c5f7735cf58
ergi98 <edervishaj98@gmail.com>
2020-12-07 13:56:49 +0100

    Attempt to add a sitemap

---
+++ public/sitemap.js
@@ -0,0 +1,14 @@
+module.exports = {
+    siteUrl: 'https://weber-soft.herokuapp.com',
+    generateRobotsTxt: true,
+    // optional
+    robotsTxtOptions: {
+      additionalSitemaps: [
+        'https://weber-soft.herokuapp.com/about',
+        'https://weber-soft.herokuapp.com/contact',
+        'https://weber-soft.herokuapp.com/tos',
+        'https://weber-soft.herokuapp.com/careers',
+        'https://weber-soft.herokuapp.com/privacy'
+      ],
+    },
+  }
